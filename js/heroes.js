d385875a47d7f8e880138f647f036ee876b9a8e6
Maria-tanina <mariyatkach2002@gmail.com>
2023-04-11 12:32:56 +0300

    modules system implement

---
+++ js/heroes.js
@@ -0,0 +1,39 @@
+export const heroes = [
+    {
+        name: 'Hulk',
+        image: 'images/heroes/hulk.png',
+        background: 'images/areas/hulk.jpg',
+        face: 'images/heroes/hulk-face.png',
+        hp: 10
+    },
+    {
+        name: 'Captain America',
+        image: 'images/heroes/captain.png',
+        background: 'images/areas/captain.jpg',
+        face: 'images/heroes/captain-face.png',
+        hp: 15
+    },
+    {
+        name: 'Ironman',
+        image: 'images/heroes/ironman.png',
+        background: 'images/areas/ironman.jpg',
+        face: 'images/heroes/ironman-face.png',
+        hp: 20
+    },
+    {
+        name: 'Spiderman',
+        image: 'images/heroes/spiderman.png',
+        background: 'images/areas/spiderman.jpg',
+        face: 'images/heroes/spiderman-face.png',
+        hp: 25
+    },
+    {
+        name: 'Venom',
+        image: 'images/heroes/venom.png',
+        background: 'images/areas/venom.jpg',
+        face: 'images/heroes/venom-face.png',
+        hp: 30
+    },
+
+
+]
